05f14c5b2e623fdb33c8ef7847a160517efab02d
unknown <cplascencia21@yahoo.com>
2018-06-06 12:26:55 -0700

    databinding-string-interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h2>{{heading}}</h2>
+    <p>
+      Name: {{person.sex === 'm' ? 'Mr' : 'Ms'}}
+      {{person.name.first + ' ' + person.name.surname}}
+    </p>
+    <p>City: {{person.city}}</p>`
 })
-export class AppComponent {
-  title = 'app';
+export class PersonComponent {
+
+  heading = 'Person Details';
+
+  person: any = {
+    name: {
+      first: 'Carlos',
+      surname: 'Plascencia'
+    },
+    sex: 'm',
+    city: 'Madera'
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { PersonComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    PersonComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [PersonComponent]
 })
 export class AppModule { }
